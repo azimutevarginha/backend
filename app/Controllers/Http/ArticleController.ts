4fd846009e63f8dbb4446175827fc3d8444bc9ca
thiagohip <thiaggohipolitomoreira@gmail.com>
2023-12-14 09:25:34 -0300

    2paginacao

--- app/Controllers/Http/ArticleController.ts
+++ app/Controllers/Http/ArticleController.ts
@@ -10,8 +10,7 @@ export default class ArticleController{
     }
     public async read({ request }: HttpContextContract) {
         const { page, perPage } = request.only(['page', 'perPage']);
-        const query = Article.query();
-        const data = await query.paginate(page || 1, perPage || 10);
+        const data = await Article.query().paginate(page || 1, perPage || 10)
     
         return data;
       }
